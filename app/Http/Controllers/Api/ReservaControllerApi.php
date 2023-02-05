46c4cfc00ffdd2aceb471e2d51ba3bfd9e533618
arc0027 <104079189+arc0027@users.noreply.github.com>
2023-02-05 12:24:44 +0100

    Restaurante

---
+++ app/Http/Controllers/Api/ReservaControllerApi.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Reserva;
+use App\Models\Reservas;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReservaControllerApi extends Controller
+{
+    public function consultarReservas()
+    {
+        $reservas = Reservas::all();
+        return response()->json($reservas);
+    }
+
+    public function insertarReserva(Request $request)
+    {
+        $reserva = new Reservas();
+        $reserva->fill($request->all());
+        $reserva->save();
+        return response()->json($reserva);
+    }
+
+    public function actualizarReserva(Request $request, $id)
+    {
+        $reserva = Reservas::findOrFail($id);
+        $reserva->fill($request->all());
+        $reserva->save();
+        return response()->json($reserva);
+    }
+
+    public function borrarReserva($id)
+    {
+        $reserva = Reservas::findOrFail($id);
+        $reserva->delete();
+        return response()->json(['mensaje' => 'Reserva eliminada']);
+    }
+}
